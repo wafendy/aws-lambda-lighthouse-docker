27bd6945a4273ab394e260dd7a9bb5ad2876cbdf
William Afendy <william_afendy@tech.gov.sg>
2021-02-18 17:53:53 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aws-lambda-lighthouse-docker
 Running Google Lighthouse on AWS Lambda using Docker
+
+#### Build and run Docker
+```
+docker build -t lighthouse .
+docker run --rm -p 9000:8080 lighthouse
+```
+
+### Test
+```
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}'
+```
---
+++ app.js
@@ -0,0 +1,64 @@
+const lighthouse = require('lighthouse');
+const chromium = require('chrome-aws-lambda');
+const log = require('lighthouse-logger');
+
+exports.lambdaHandler = async (event) => {
+  let browser;
+  let response;
+  log.setLevel("error");
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: [...chromium.args, "--remote-debugging-port=9222"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    const options = {
+      output: "json",
+      preset: 'desktop',
+      onlyCategories: ["performance", "seo", "accessibility", "best-practices"],
+      port: 9222,
+    }
+
+    const url = 'https://www.google.com';
+
+    const result = await lighthouse(url, options);
+    console.log(`Audited ${url} in ${result.lhr.timing.total} ms.`);
+
+    const report = JSON.parse(result.report);
+
+    console.log('P: ', report['categories']['performance']['score']);
+    console.log('A: ', report['categories']['accessibility']['score']);
+    console.log('S: ', report['categories']['seo']['score']);
+    console.log('B: ', report['categories']['best-practices']['score']);
+    
+    response = {
+      statusCode: 200,
+      body: {
+        'Perfomance': report['categories']['performance']['score'],
+        'Accessibility': report['categories']['accessibility']['score'],
+        'SEO': report['categories']['seo']['score'],
+        'BestPractices': report['categories']['best-practices']['score'],
+        'ErrorMessage': report['audits']['speed-index']['errorMessage']
+      },
+      report: report
+    }
+
+  } catch (error) {
+    console.error(error);
+
+    response = {
+      statusCode: 500,
+      body: error
+    }
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return response;
+};
